1c98b8e705c8b0c66af876b2f81e8abe110f1ec7
LeanKhan <eslean20@gmail.com>
2021-03-17 13:34:30 +0100

    fix ending_at not added to starting_at

--- app/static/js/election.js
+++ app/static/js/election.js
@@ -1,6 +1,7 @@
 if (document.getElementById("datetime_picker")) {
   const times_input = document.getElementById("times_input");
   const unit_select = document.getElementById("unit_select");
+  const starting_at_input = document.getElementById("starting_at");
   const ending_at_input = document.getElementById("ending_at");
   const ending_at_display = document.getElementById("ending_at_display");
   const datetime_input = document.getElementById("datetime_input");
@@ -10,13 +11,58 @@ if (document.getElementById("datetime_picker")) {
   // we likewise change the max on the times_input, then we change the value of the ending_at input to
   // the corresponding value
 
+  starting_at_input.addEventListener("input", function (e) {
+    // reset the whole time thing...
+
+    // debugger;
+
+    // var last_ending_at_value = ending_at_input.value;
+    // setInterval(function () {
+    //   var newValue = ending_at_input.value;
+    //   if (last_ending_at_value != newValue) {
+    //     last_ending_at_value = newValue;
+    //     handleValueChange();
+    //   }
+    // }, 50); // 20 times/second
+    // function handleValueChange() {
+    //   console.log("$input's value changed: " + ending_at_input.value);
+    // }
+
+    const time = moment(e.target.value).add(
+      times_input.value,
+      unit_select.value
+    );
+
+    // Rounding minute on 30 mins interval
+    // from... https://stackoverflow.com/a/61117519/10382407
+    // if (minute <= 30 && unit_select.value != "m") time.set({ minute: 30 });
+    // if (minute > 30 && unit_select.value != "m") time.set({ minute: 0 });
+
+    if (
+      unit_select.value != "m" &&
+      unit_select.value != "h" &&
+      datetime_input.value
+    ) {
+      time.set({
+        hour: datetime_input.value.split(":")[0],
+        minute: datetime_input.value.split(":")[1],
+      });
+    }
+
+    ending_at_input.value = time.format("Y-MM-DDTHH:mm");
+
+    ending_at_display.innerText = "On " + time.format("lll");
+  });
+
   unit_select.addEventListener("change", function (e) {
+    // debugger;
+
     times_input.value = "";
     ending_at_input.value = "";
     ending_at_display.innerText = "";
 
     if (e.target.value != "m" && e.target.value != "h") {
-      datetime_input_block.style.display = "inline-block";
+      datetime_input_block.style.display = "flex";
     } else {
       datetime_input_block.style.display = "none";
     }
@@ -56,33 +102,54 @@ if (document.getElementById("datetime_picker")) {
   });
 
   datetime_input.addEventListener("input", function (e) {
-    if (e.target.value != "m" && e.target.value != "h") {
-      const time = moment(ending_at_input.value);
+    if (unit_select.value != "m" && unit_select.value != "h") {
+      // debugger;
 
-      const datetime_input_time = datetime_input.value;
+      let time = moment(ending_at_input.value);
+
+      let datetime_input_time = datetime_input.value;
 
       time.set({
         hour: datetime_input_time.split(":")[0],
         minute: datetime_input_time.split(":")[1],
       });
 
-      ending_at_input.value = time.format("Y-MM-DDTH:mm");
+      ending_at_input.value = time.format("Y-MM-DDTHH:mm");
 
       ending_at_display.innerText = "On " + time.format("lll");
     }
   });
 
+  // TODO: llok at that alpine.js mutation obeerbr o
+
   times_input.addEventListener("input", function (e) {
     // when the user inputs a new time...
+    // debugger;
 
-    const time = moment().add(e.target.value, unit_select.value);
+    const starting_at_time = starting_at_input.value || new Date();
+
+    const time = moment(starting_at_time).add(
+      e.target.value,
+      unit_select.value
+    );
 
     // Rounding minute on 30 mins interval
     // from... https://stackoverflow.com/a/61117519/10382407
     // if (minute <= 30 && unit_select.value != "m") time.set({ minute: 30 });
     // if (minute > 30 && unit_select.value != "m") time.set({ minute: 0 });
 
-    ending_at_input.value = time.format("Y-MM-DDTH:mm");
+    if (
+      unit_select.value != "m" &&
+      unit_select.value != "h" &&
+      datetime_input.value
+    ) {
+      time.set({
+        hour: datetime_input.value.split(":")[0],
+        minute: datetime_input.value.split(":")[1],
+      });
+    }
+
+    ending_at_input.value = time.format("Y-MM-DDTHH:mm");
 
     ending_at_display.innerText = "On " + time.format("lll");
   });
